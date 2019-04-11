8c7d47208668e6dad84aa8af20b61c002bb1e4d9
Andrii Veklychev <wzooff@gmail.com>
2019-04-11 22:13:57 +0300

    Update update-metadata.py

--- update-metadata.py
+++ update-metadata.py
@@ -1,20 +1,42 @@
-#!/usr/local/bin/python3
-# Will update cookbook versions to latest, that exist on chef-server
-
-# pychef should be installed
+#!/usr/bin/python3.6
 
 import chef
 import fileinput
 import re
+import os
+import sys
+import getopt
 
-api = chef.autoconfigure()
-cookbooks = api['/cookbooks']
+def main(argv):
+   inputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"i:",["ifile="])
+      if not opts:
+         print('Usage: metadata.py -i <path_to_metadata.rb>')
+         sys.exit(2)
+   except getopt.GetoptError:
+      print('Usage: metadata.py -i <path_to_metadata.rb>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt in ("-i", "--ifile"):
+         inputfile = arg
+      else:
+         print ('Usage: metadata.py -i <path_to_metadata.rb>')
+         sys.exit()
+      
+      input_path = os.path.abspath(inputfile)
 
-with fileinput.FileInput('metadata.rb', inplace=True) as file:
-    for line in file:
-        dependsfound = re.search(r"depends \'(.*)\',", line)
-        if dependsfound:
-            version = cookbooks[dependsfound.group(1)]['versions'][0]['version']
-        else:
-            version = ''
-        print(re.sub(r"~> (.*)\'", "~> %s'" % version, line), end='')
+      api = chef.autoconfigure()
+      cookbooks = api['/cookbooks']
+
+      with fileinput.FileInput(input_path, inplace=True) as file:
+          for line in file:
+              dependsfound = re.search(r"depends \'(.*)\',", line)
+              if dependsfound:
+                  version = cookbooks[dependsfound.group(1)]['versions'][0]['version']
+              else:
+                  version = ''
+              print(re.sub(r"~> (.*)\'", "~> %s'" % version, line), end='')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
